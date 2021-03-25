ba9a94034c76c660392e928520667329b1441978
Holden Bouwers <bouwersholden@gmail.com>
2021-03-25 19:22:23 -0400

    add details functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -101,5 +101,22 @@ namespace RestaurantRater.Controllers
             }
             return View(restaurant);
         }
+
+        // GET: Restaurant/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+
+            if (restaurant == null)
+            {
+                return new HttpNotFoundResult();
+            }
+            return View(restaurant);
+        }
     }
 }
